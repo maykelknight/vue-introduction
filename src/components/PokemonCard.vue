3740915fb1a341aa1cf382259c7ef77bb5fdb088
Michał Czapkowicz <czapkowicz@nonstop.co>
2020-08-06 22:49:58 +0200

    PokemonCard added

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="card">
+        <img :src="details.image" class="card__image"/>
+        <p class="card__name">{{details.name}}</p>
+        <p class="card__id">#{{details.id}}</p>
+        <ul class="card__types">
+            <li v-for="type in details.types">
+                {{type}}
+            </li>
+        </ul>
+        <button class="card__btn">Check Details</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Pokemon.vue",
+    props: {
+        details: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .card {
+        width: 200px;
+        height: 250px;
+        border-radius: 10px;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        background: #eee;
+
+        &__name {
+            text-transform: capitalize;
+            font-size: 20px;
+            margin-top: -10px;
+        }
+
+        &__id {
+            margin-bottom: 5px;
+            margin-top: 25px;
+        }
+        &__types {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+
+            li {
+                padding: 5px 10px;
+                background: burlywood;
+                margin: 5px;
+                border-radius: 5px;
+                font-size: 12px;
+            }
+        }
+
+        &__btn {
+            margin-top: 10px;
+            border: 1px solid black;
+            border-radius: 5px;
+            padding: 5px 10px;
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <PokemonCard :details="pokemon"
+                   v-for="pokemon in sortedList"
+      ></PokemonCard>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PokemonCard from "@/components/PokemonCard.vue"
 export default {
   name: 'Home',
+  data() {
+    return {
+      pokemonList: []
+    }
+  },
+  computed: {
+    sortedList() {
+      return this.pokemonList.sort((a,b) => a.id - b.id)
+    }
+  },
+  created() {
+    for (let i = 1; i <= 50; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      fetch(url)
+              .then(res => res.json())
+              .then(pokemon => {
+                this.pokemonList.push({
+                  name: pokemon.name,
+                  id: pokemon.id,
+                  types: pokemon.types.map(type => type.type.name),
+                  image: pokemon.sprites.front_default
+                });
+              });
+    }
+  },
   components: {
-    HelloWorld
+    PokemonCard
   }
 }
 </script>
+<style scoped lang="scss">
+  .home {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 1000px;
+    margin: 0 auto;
+  }
+</style>
